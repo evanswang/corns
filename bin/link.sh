56d65e8f31d5d543d149989cb60962e3216e2f67
Shicai Wang <sw23@sanger.ac.uk>
2020-05-02 23:39:18 +0100

    update

--- bin/link.sh
+++ bin/link.sh
@@ -8,8 +8,10 @@
 
 for((i=0;i<$((${NODE_NUM}));i++))
 do
-  for((j=0;j<$((${NODE_NUM}));j++))
-  do
-    ln -s ${RES}/block_${i}_${j} ${RES}/block_${j}_${i} 
-  done
+	for((j=0;j<$((${NODE_NUM}));j++))
+	do
+		if [[ ! -f ${RES}/block_${j}_${i} ]]; then
+			ln -s ${RES}/block_${i}_${j} ${RES}/block_${j}_${i} 
+		fi
+	done
 done
